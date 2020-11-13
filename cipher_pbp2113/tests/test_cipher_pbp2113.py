2a7967fcf299befc45e9308926b538d3296d495d
Paul Platzman <pbp2113@columbia.edu>
2020-11-13 18:02:05 -0500

    Adding a test and pytest

--- cipher_pbp2113/tests/test_cipher_pbp2113.py
+++ cipher_pbp2113/tests/test_cipher_pbp2113.py
@@ -1,5 +1,11 @@
 from cipher_pbp2113 import __version__
 from cipher_pbp2113 import cipher_pbp2113
+import pytest
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_cipher_negative_shift():
+    actual = cipher_pbp2113.cipher("Johnny",-5)
+    expected = 'Ejciit'
+    assert actual == expected
